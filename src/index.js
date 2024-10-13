7edf96f4e0148953e656cba016ad9356a5852dc1
Matt Rangel <matt333three@aol.com>
2024-10-13 10:00:28 -0500

    Import tab loaders to index page and implement switch tab function. Add click listener to tab buttons to call switch tab function.

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import loadHomepage from "./load-homepage.js";
+import loadMenu from "./load-menu.js";
+import loadContact from "./load-contact.js";
+import loadAbout from "./load-about.js";
+
+function switchTab(tab) {
+  document.querySelector("#content").replaceChildren();
+  switch(tab) {
+    case "homepage":
+      loadHomepage();
+      break;
+    case "menu":
+      loadMenu();
+      break;
+    case "contact":
+      loadContact();
+      break;
+    case "about":
+      loadAbout();
+      break;
+    default:
+      break;
+  }
+};
+
+function addListenersToTabButtons() {
+  const tabButtons = document.querySelectorAll(".tab-switch");
+  for (const button of tabButtons) {
+    button.addEventListener("click", (e) => { 
+      switchTab(e.currentTarget.value);
+    });
+  }
+};
+
+loadHomepage();
+addListenersToTabButtons();
